be1a51be4b61f295aa2b217c356321410746e84e
JawsGeun <whtnrms2012@naver.com>
2018-07-21 12:45:28 +0900

    간단 버튼 토스트 구현 해봄

--- app/src/main/java/org/woolrim/woolrim/MainActivity.java
+++ app/src/main/java/org/woolrim/woolrim/MainActivity.java
@@ -2,6 +2,8 @@ package org.woolrim.woolrim;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnClick(View v) {
+        Toast.makeText(getApplicationContext(),"클릭",Toast.LENGTH_LONG).show();
     }
 }
