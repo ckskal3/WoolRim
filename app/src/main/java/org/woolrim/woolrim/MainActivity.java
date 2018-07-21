ea3ed0b71ab4b5d0439f741891d91835f343b5f1
JawsGeun <whtnrms2012@naver.com>
2018-07-21 14:33:17 +0900

    m4a 파일 ( 보통 모바일에서 녹음하면 생성되는 확장자 ) 을 통한 재생, 일시정지, 정지 기능과 Seek Bar 표시

--- app/src/main/java/org/woolrim/woolrim/MainActivity.java
+++ app/src/main/java/org/woolrim/woolrim/MainActivity.java
@@ -1,19 +1,75 @@
 package org.woolrim.woolrim;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
+import java.io.IOException;
+/*
+    android media player with seek bar
+    created by 수근
+ */
 public class MainActivity extends AppCompatActivity {
+    private MediaPlayer mediaPlayer;
+    private boolean isPlaying = false;
+    private SeekBar seekBar;
+
+    class SeekBarThread extends Thread {
+        @Override
+        public void run() {
+            while (isPlaying) {
+                seekBar.setProgress(mediaPlayer.getCurrentPosition());
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        seekBar = findViewById(R.id.seekBar_progress);
+        mediaPlayer = MediaPlayer.create(this, R.raw.voice_test);
+        seekBar.setMax(mediaPlayer.getDuration());
     }
 
-    public void onBtnClick(View v) {
-        Toast.makeText(getApplicationContext(),"클릭",Toast.LENGTH_LONG).show();
+    @Override
+    protected void onPause() {
+        super.onPause();
+        isPlaying = false;
+        if (mediaPlayer!=null) {
+            mediaPlayer.release();
+        }
+    }
+
+    public void onStartBtnClick(View v) {
+        Toast.makeText(getApplicationContext(), "재생", Toast.LENGTH_LONG).show();
+        mediaPlayer.start();
+        isPlaying = true;
+        new SeekBarThread().start();
+    }
+
+    public void onPauseBtnClick(View v) {
+        Toast.makeText(getApplicationContext(), "일시정지", Toast.LENGTH_LONG).show();
+        mediaPlayer.pause();
+        isPlaying = false;
+    }
+
+    public void onStopBtnClick(View v) {
+        Toast.makeText(getApplicationContext(), "정지", Toast.LENGTH_LONG).show();
+        mediaPlayer.stop();
+        isPlaying = false;
+        seekBar.setProgress(0);
+        try {
+            mediaPlayer.prepare();
+            mediaPlayer.seekTo(0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void onClick(View v) {
+        finish();
     }
 }
