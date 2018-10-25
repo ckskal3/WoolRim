0774c455f1a072760d4eb0b2e78536fd8d8353ec
whtnrms2017 <sugeun.jo@croquis.com>
2018-10-25 11:04:22 +0900

    production setting

--- server/src/index.js
+++ server/src/index.js
@@ -32,10 +32,10 @@ app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
-app.get('/graphql', graphqlHTTP({
-  schema,
-  graphiql:true
-}));
+// app.get('/graphql', graphqlHTTP({
+//   schema,
+//   graphiql:true
+// }));
 
 app.post('/graphql', graphqlHTTP({
   schema,
--- server/src/model/connect_db.js
+++ server/src/model/connect_db.js
@@ -1,16 +1,17 @@
 import { Connection } from 'cormo';
 
-// export const Connection = new cormo.Connection('mysql', { 
-//     host: 'woolrimdb.cbb7lmv7tjj3.ap-northeast-2.rds.amazonaws.com',
-//     port: 3306,
-//     user: 'whtnrms2018',
-//     password: 'dnfflaelql',
-//     database: 'woolrim_proj',
-// });
-export const Conn = new Connection('mysql', {
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'Tnrms@@852',
-  database: 'woolrim',
+export const Conn = new Connection('mysql', { 
+    host: 'woolrim-db.czpqx7iovvxj.ap-northeast-2.rds.amazonaws.com',
+    port: 3306,
+    user: 'woolrim_admin',
+    password: 'dnfflawoolrim',
+    database: 'woolrim_db',
 });
+
+// export const Conn = new Connection('mysql', {
+//   host: 'localhost',
+//   port: 3306,
+//   user: 'root',
+//   password: 'Tnrms@@852',
+//   database: 'woolrim',
+// });
