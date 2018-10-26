ddd11ac20e3e29d83a99d0896e8201c1798953f8
whtnrms2017 <sugeun.jo@croquis.com>
2018-10-26 13:53:04 +0900

    웹 서버 연동 테스트2

---
+++ web/web_server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname,'build')))
+
+app.listen(5000, function(req, res){
+  console.log('5000 포트 개방!')
+  console.log(path.join(__dirname,'build'));
+});
