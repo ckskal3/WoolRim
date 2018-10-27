7a063919ec86fec5cddba3e8824f77fba58bea84
EC2 Default User <ec2-user@ip-172-31-31-201.ap-northeast-2.compute.internal>
2018-10-27 05:39:52 +0000

    웹 서버 pm2 셋팅

--- web/web_server.js
+++ web/web_server.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')))
 
+app.get('/*', function (req, res) {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.listen(5000, function (req, res) {
   console.log('5000 포트 개방!')
   console.log(path.join(__dirname, 'build'));
