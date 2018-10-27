8e73c95d9bfddc3cd4ba7cbcebfb6234502f0517
whtnrms2017 <sugeun.jo@croquis.com>
2018-10-27 14:22:42 +0900

    잔기능 수정

--- web/src/serverInfo.js
+++ web/src/serverInfo.js
@@ -1,6 +1,6 @@
 const serverInfo = {
   // serverURL: 'http://localhost:3000/graphql', // 개발용
-  serverURL: '52.79.33.194:3000/graphql', // 실제 서버
+  serverURL: 'http://52.79.33.194:3000/graphql', // 실제 서버
 }
 
 export default serverInfo;
