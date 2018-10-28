01592402d94b9a5bcefe5375ab26cf3be41f38ff
EC2 Default User <ec2-user@ip-172-31-31-201.ap-northeast-2.compute.internal>
2018-10-28 12:28:14 +0000

    잔기능 수정

--- web/src/serverInfo.js
+++ web/src/serverInfo.js
@@ -1,8 +1,9 @@
 const serverInfo = {
-  serverURL: 'http://localhost:3000/graphql', // 개발용 서버
-  // serverURL: 'http://52.79.33.194:3000/graphql', // 실제 서버
-  webServerURL: 'http://localhost:5000' // 개발용 웹 서버
-  // serverURL: 'http://52.79.33.194:5000', // 실제 웹 서버
+  serverURL: 'http://52.79.33.194:3000/graphql', // 실제 서버
+  webServerURL: 'http://52.79.33.194:5000', // 실제 웹 서버
+  
+  // serverURL: 'http://localhost:3000/graphql', // 개발용 서버
+  // webServerURL: 'http://localhost:5000' // 개발용 웹 서버
 }
 
 export default serverInfo;
