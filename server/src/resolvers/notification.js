9e251d29cb5a0281dbfc9665280b0ba3d6911cdc
whtnrms2017 <sugeun.jo@croquis.com>
2018-11-09 20:45:16 +0900

    server - 사용자 별로 안읽은 알림 갯수 알려주는 기능 구현

--- server/src/resolvers/notification.js
+++ server/src/resolvers/notification.js
@@ -37,6 +37,14 @@ const getNotification = async (stu_id) => {
   }
 }
 
+const getUnreadCount = async (user_id) => {
+  try{
+    return await Notification.where({user_id, read_flag: false}).count();
+  }catch(err){
+    return -1;
+  }
+}
+
 const createNotification = async (input_list) => {
   input_list.map(item => {
     item.created = new Date();
@@ -71,6 +79,7 @@ const deleteNotification = async (id_list) => {
 const notificationResolver = {
   Query: {
     getNotification: (obj, { stu_id }) => getNotification(stu_id),
+    getUnreadCount: (obj, { user_id }) => getUnreadCount(user_id),
   },
   Mutation: {
     createNotification: (obj, { input_list }) => createNotification(input_list),
