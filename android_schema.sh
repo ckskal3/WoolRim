fdbd939fce75f659d208e6e9d373d6665063b90d
whtnrms2017 <sugeun.jo@croquis.com>
2018-11-29 22:36:42 +0900

    안드로이드 스키마 설정 파일 수정

--- android_schema.sh
+++ android_schema.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-apollo-codegen download-schema http://52.79.33.194:3000/graphql ./
+apollo-codegen download-schema http://13.125.221.121:3000/graphql ./
+git add ./schema.json
+git commit -m '안드로이드 스키마 파일 업로드'
