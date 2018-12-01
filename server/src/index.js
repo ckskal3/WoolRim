d7fc42cebb609c0f2e482f29708b84f919c44626
whtnrms2017 <sugeun.jo@croquis.com>
2018-12-01 19:45:52 +0900

    server - 합성 로직 변경

--- server/src/index.js
+++ server/src/index.js
@@ -5,13 +5,14 @@ import cors from 'cors';
 import fs from 'fs';
 import path from 'path';
 import mixAudio from './mixAudio';
+import bodyParser from 'body-parser';
 
 const multer = require('multer');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    const dir = path.join(__dirname,`../../../woolrim_storage/${req.body.stu_id}/`)
-    if(!fs.existsSync(dir)){
+    const dir = path.join(__dirname, `../../../woolrim_storage/${req.body.stu_id}/`)
+    if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir);
     }
     cb(null, dir)
@@ -26,7 +27,9 @@ const upload = multer({ storage: storage });
 const apiServer = express();
 const fileServer = express();
 
-fileServer.use(express.static(path.join(__dirname,'../../../woolrim_storage/')));
+fileServer.use(bodyParser.json());
+fileServer.use(bodyParser.urlencoded({ extended: true }));
+fileServer.use(express.static(path.join(__dirname, '../../../woolrim_storage/')));
 fileServer.use(cors());
 fileServer.set('view engine', 'jade')
 fileServer.set('views', __dirname + '/templates')
@@ -51,7 +54,36 @@ apiServer.listen(3000, () => {
   console.log('3000번 api 서버 포트 개방!!');
 });
 
+fileServer.post('/mix', async function (req, res) {
+  console.log(req.body); // mix_num, stu_id, file_name
+  if (req.body.mix_num) {
+    console.time('mix_time')
+    if (await mixAudio(req.body.stu_id, req.body.file_name, req.body.mix_num)) {
+      console.timeEnd('mix_time')
+      res.status(200).send('success');
+      console.log('학번: ', req.body.stu_id, ', 파일이름: ', req.body.file_name, ' 합성 성공');
+    } else {
+      console.timeEnd('mix_time')
+      res.status(500).send('mix_fail');
+      console.log('학번: ', req.body.stu_id, ', 파일이름: ', req.body.file_name, ' 합성 실패');
+    }
+  } else {
+    const file_name_without_type = req.body.file_name.replace(/\..+$/, '').trim();
+    const path = path.join(__dirname, `../../../woolrim_storage/${req.body.stu_id}/${file_name_without_type}_0.mp3`)
+    if (fs.existsSync(path)) {
+      res.status(200).send('success');
+    } else {
+      res.status(404).send('file_not_found')
+    }
+  }
+});
+
 fileServer.post('/upload', upload.single('user_recording'), async function (req, res) {
+  if (fs.existsSync(req.file.path)) {
+    res.status(200).send('success');
+  } else {
+    res.status(500).send('fail');
+  }
   // console.log(req.body); // stu id
   // console.log(req.file)
   // { fieldname: 'user_recording',
@@ -62,27 +94,19 @@ fileServer.post('/upload', upload.single('user_recording'), async function (req,
   // filename: 'AudioTest.aac',
   // path: '/Users/jaws/privateProj/woolrim_storage/555555/AudioTest.aac',
   // size: 571370 }
-  
-  console.time('mix_time')
-  if(await mixAudio(req.body.stu_id, req.file.originalname)){
-    console.timeEnd('mix_time')
-    res.status(200).send('success');
-    console.log('학번: ',req.body.stu_id,', 파일이름: ',req.file.originalname,' 합성 성공');
-    const file_name_without_type = req.file.filename.replace(/\..+$/, '').trim();
-    fs.renameSync(req.file.path, `${req.file.destination}${file_name_without_type}_0.mp3`);
-  }else{
-    console.timeEnd('mix_time')
-    res.status(500).send('fail');
-    console.log('학번: ',req.body.stu_id,', 파일이름: ',req.file.originalname,' 합성 실패');
-  }
+
 });
 
 fileServer.get('/upload', function (req, res) {
   res.render('upload');
 });
 
-fileServer.get('/:id/:filename' ,function(req, res) {
-  res.sendFile(path.join(__dirname,`../../../woolrim_storage/${req.params.id}/${req.params.filename}`))
+fileServer.get('/mix', function (req, res) {
+  res.render('mix');
+});
+
+fileServer.get('/:id/:filename', function (req, res) {
+  res.sendFile(path.join(__dirname, `../../../woolrim_storage/${req.params.id}/${req.params.filename}`))
   // res.sendFile(path.join(__dirname,`../uploads/${req.params.id}/${req.params.filename}`))
 })
 
--- server/src/mixAudio.js
+++ server/src/mixAudio.js
@@ -10,27 +10,43 @@ const background_paths = [
   path.join(__dirname, '../../../woolrim_storage/background_music/3.mp3'), // 어두운
 ];
 
-const mixAudio = async (stu_id, file_name) => {
+const mixAudio = async (stu_id, file_name, mix_num) => {
   const recording_path = path.join(__dirname + `../../../../woolrim_storage/${stu_id}/${file_name}`);
   const file_name_without_type = file_name.replace(/\..+$/, '').trim();
   if (!fs.existsSync(recording_path)) {
     console.log(`${recording_path} NOT FOUND`)
     return false;
   }
-  let results = [];
-  await Promise.all(background_paths.map(async (back, i) => {
-    if (!fs.existsSync(back)) { return; }
-    const result_path = path.join(__dirname, `../../../woolrim_storage/${stu_id}/${file_name_without_type}_${i + 1}.mp3`);
-    results.push(result_path);
-    if (fs.existsSync(result_path)) {
-      fs.unlinkSync(result_path);
-    }
-    await exec(`ffmpeg -i ${recording_path} -i ${back} -filter_complex amerge ${result_path}`);
-  }));
-  return results.reduce((accumulator, currentValue) => {
-    if (!fs.existsSync(currentValue)) { return accumulator && false; }
-    return accumulator;
-  }, true);
+  const background = background_paths[mix_num - 1];
+  if (!background) {
+    console.log(`${mix_num} background file not exist`);
+    return false;
+  }
+  const result_path = path.join(__dirname, `../../../woolrim_storage/${stu_id}/${file_name_without_type}_${mix_num}.mp3`);
+  if (fs.existsSync(result_path)) {
+    fs.unlinkSync(result_path);
+  }
+  await exec(`ffmpeg -i ${recording_path} -i ${background} -filter_complex amerge ${result_path}`);
+  if (fs.existsSync(result_path)) {
+    return true;
+  }else{
+    console.log('mix failed');
+    return false;
+  }
+  // let results = [];
+  // await Promise.all(background_paths.map(async (back, i) => {
+  //   if (!fs.existsSync(back)) { return; }
+  //   const result_path = path.join(__dirname, `../../../woolrim_storage/${stu_id}/${file_name_without_type}_${i + 1}.mp3`);
+  //   results.push(result_path);
+  //   if (fs.existsSync(result_path)) {
+  //     fs.unlinkSync(result_path);
+  //   }
+  //   await exec(`ffmpeg -i ${recording_path} -i ${back} -filter_complex amerge ${result_path}`);
+  // }));
+  // return results.reduce((accumulator, currentValue) => {
+  //   if (!fs.existsSync(currentValue)) { return accumulator && false; }
+  //   return accumulator;
+  // }, true);
 }
 
 export default mixAudio
